fe58860cb2bbd6b33a2cc269af6a55d7db557391
Doni <donilan.qq@gmail.com>
2014-01-09 13:50:21 +0800

    Update some code.

--- js/calendar.js
+++ js/calendar.js
@@ -1,50 +1,52 @@
-(function($){
+var Calendar = function(_opts){
     var defaultOptions = {
+        id: null,
+        debug: true,
         weekDaynames: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
         tableTmpl: '<table cellpadding="0" cellspacing="0" ></table>',
         onclick: function(){}
     };
-    $.fn.calendar = function(options) {
-        options = $.extend({}, defaultOptions, options)
-        this.each(function(){
-            var $that = $(this);
-            $that.addClass('calendar-container');
-            $that.height($(window).height());
-            var $cal = calendar($that, options);
-        });
-    };
-
-    $(window).resize(function(){
-        if($(window).height() > 300)
-            $('.calendar-container').height($(window).height());
-    });
+    this.options = $.extend({}, defaultOptions, _opts);
+    this.data = {};
+    this.cells = [];
+    /* Init function*/
+    this._init = function() {
+        this.debug('Element id [' + this.options.id + '] begin init...');
+        this.calendar = $('#'+this.options.id);
+        this.calendar.html('')
+            .addClass('calendar-container')
+            .height($(window).height());
+        this._makeCalendarTop();
+        this._makeMonthCells();
+        this.debug('Init done.')
+    }
 
     /**
      * Make top elemnts for calendar.
      */
-    function makeCalendarTop($wrapper, opts) {
+    this._makeCalendarTop = function() {
         var $div = $('<div class="week-daynames"></div>');
-        var $table = $(opts.tableTmpl);
-        $wrapper.append($div);
+        var $table = $(this.options.tableTmpl);
+        this.calendar.append($div);
         $div.append($table);
         $tr = $('<tr></tr>');
         $table.append($tr);
-        for(var i = 0; i < opts.weekDaynames.length; ++i) {
-            $tr.append('<th title="'+opts.weekDaynames[i]+'">' +
-                       opts.weekDaynames[i] + '</th>');
+        for(var i = 0; i < this.options.weekDaynames.length; ++i) {
+            $tr.append('<th title="'+this.options.weekDaynames[i]+'">' +
+                       this.options.weekDaynames[i] + '</th>');
         }
-        return $table;
     }
-    function makeMonthCells($wrapper, opts) {
+
+    this._makeMonthCells = function() {
         var $eventContainer = $('<div class="event-container"></div>');
-        $wrapper.append($eventContainer);
+        this.calendar.append($eventContainer);
         for(var y = 0; y < 5; ++y) {
             $monthRow = $('<div class="month-row"></div>');
             $monthRow.css('top', y*20 + '%');
             $monthRow.css(y == 4? 'bottom': 'height', y == 4? '0':'21%');
             $eventContainer.append($monthRow);
             $tr = $('<tr></tr>');
-            $table = $(opts.tableTmpl).append($tr);
+            $table = $(this.options.tableTmpl).append($tr);
             $table.addClass('month-row-table');
             $monthRow.append($table);
 
@@ -52,16 +54,22 @@
                 $tr.append('<td>&nbsp;</td>');
             }
         }
-        return [];
     }
-    /**
-     * make a stage for month calendar.
-     */
-    function calendar(calendarEl, opts) {
-        $cal = $(calendarEl);
-        $cal.html('');
-        $cal.daynamesTable = makeCalendarTop($cal, opts);
-        $cal.cells = makeMonthCells($cal, opts);
-        return $cal;
+
+    /* debug message */
+    this.debug = function(msg) {
+        if(this.options.debug) {
+            console.log(msg);
+        }
     }
-})(jQuery);
+
+    if(this.options.id)
+        this._init(_opts);
+    else
+        this.debug('Options id cannot be null or empty.');
+        
+};
+$(window).resize(function(){
+    if($(window).height() > 300)
+        $('.calendar-container').height($(window).height());
+});
